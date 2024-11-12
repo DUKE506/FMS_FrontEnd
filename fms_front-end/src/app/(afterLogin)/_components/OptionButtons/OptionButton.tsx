1a9b4621391dc714e7a09fb18537dbdc94600276
DUKE506 <dukebs1128@gmail.com>
2024-11-12 23:12:38 +0900

    feat : OptionsButtons 작업 중

--- fms_front-end/src/app/(afterLogin)/_components/OptionButtons/OptionButton.tsx
+++ fms_front-end/src/app/(afterLogin)/_components/OptionButtons/OptionButton.tsx
@@ -3,15 +3,21 @@ import Delete from '../../../../../public/images/trash.svg'
 import Edit from '../../../../../public/images/pencil-square.svg'
 import Options from '../../../../../public/images/three-dots-vertical.svg'
 import { useState } from 'react'
-
+import styles from './OtionButton.module.css'
 
 export const OptionButton = () =>{
     const [active, setActive] = useState<boolean>(false);
     const [isWork, setIsWork] = useState<boolean>(false);
+
+    const handleOptionsClick = () =>{
+        setActive(!active)
+    }
+
     return(
-        <div>
+        <div className={styles.container}>
             <Options 
             style={{ fill: '#606060', width: 'var(--size-icon)', height: 'var(--size-icon)' }}
+            onClick={handleOptionsClick}
             />
             {
                 active ?
@@ -26,15 +32,30 @@ export const OptionButton = () =>{
 //옵션 : 추가, 수정, 삭제
 const OptionsList = () => {
     return(
-        <ul>
-            <li>
-                <Add/>
+        <ul className={`${styles.col} ${styles.list}`}>
+            <li className={`${styles.row}`}>
+                <Add 
+                style={{ fill: '#606060', width: 'var(--size-icon)', height: 'var(--size-icon)' }}
+                />
+                <span className={`${styles.text}`}>
+                    추가
+                </span>
             </li>
-            <li>
-                <Edit/>
+            <li className={`${styles.row}`}>
+                <Edit
+                style={{ fill: '#606060', width: 'var(--size-icon)', height: 'var(--size-icon)' }}
+                />
+                <span className={`${styles.text}`}>
+                    수정
+                </span>
             </li>
-            <li>
-                <Delete/>
+            <li className={`${styles.row}`}>
+                <Delete
+                style={{ fill: '#606060', width: 'var(--size-icon)', height: 'var(--size-icon)' }}
+                />
+                <span className={`${styles.text}`}>
+                    삭제
+                </span>
             </li>
         </ul>
     )
